9a7099bd4e17d81aee7d9742f6004f3a184586fa
Theodore Halpern <theohal@meta.com>
2024-06-18 13:08:26 -0700

    feat: Added express with TS

--- Kudos-Board-Backend/index.ts
+++ Kudos-Board-Backend/index.ts
@@ -1,18 +1,29 @@
 import { PrismaClient } from '@prisma/client'
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 const prisma = new PrismaClient()
 
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+// ts setup from https://blog.logrocket.com/how-to-set-up-node-typescript-express/
 async function main() {
-    const allUsers = await prisma.user.findMany()
-    console.log(allUsers)
+    app.get("/", (req: Request, res: Response) => {
+        res.send("Express + TypeScript Server");
+    });
+
+    app.listen(port, () => {
+        console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
