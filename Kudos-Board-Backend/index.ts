dd09223030d30e216087855846c8d9f4cdad1f92
Theodore Halpern <theohal@meta.com>
2024-06-18 16:39:44 -0700

    feat: added backend paths to create and view data

--- Kudos-Board-Backend/index.ts
+++ Kudos-Board-Backend/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
@@ -7,26 +7,73 @@ const prisma = new PrismaClient()
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.get("/boards", async (req: Request, res: Response) => {
+  const boards = await prisma.board.findMany()
+  res.json(boards)
+});
+
+app.post('/boards', async (req: Request, res: Response) => {
+  const { title, imageUrl } = req.body
+  const newBoard = await prisma.board.create({
+    data: <Prisma.BoardCreateInput> {
+      title,
+      imageUrl
+    },
+  })
+  res.json(newBoard)
+})
+
+app.get("/board/:id/posts", async (req: Request, res: Response) => {
+  const boardId = parseInt(req.params.boardId)
+  const boards = await prisma.board.findUnique({
+    where: <Prisma.BoardWhereUniqueInput> {
+      id: boardId,
+    },
+    select: <Prisma.BoardSelect> {
+      title: true,
+      createdAt: true,
+      posts: true,
+      imageUrl: true,
+    },
+  })
+  res.json(boards)
+});
+
+app.post('/board/:boardId/posts', async (req: Request, res: Response) => {
+  const { title, imageUrl } = req.body
+  const boardId = parseInt(req.params.boardId)
+  const updatedBoard = await prisma.board.update({
+    where: { id: boardId },
+    data: <Prisma.BoardUpdateInput>  {
+      profile: {
+        create: <Prisma.PostCreateInput> {
+          title: title,
+          imageUrl: imageUrl,
+        },
+      },
+    },
+  })
+  res.json(updatedBoard)
+})
+
 // express ts setup from https://blog.logrocket.com/how-to-set-up-node-typescript-express/
 async function main() {
-    app.get("/", (req: Request, res: Response) => {
-        res.send("Express + TypeScript Server");
-    });
-
-
-
-
-    app.listen(port, () => {
-        console.log(`[server]: Server is running at http://localhost:${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- Kudos-Board-Backend/prisma/migrations/20240618224018_init/migration.sql
+++ Kudos-Board-Backend/prisma/migrations/20240618224018_init/migration.sql
@@ -5,8 +5,9 @@ CREATE TABLE "Post" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "content" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-    "authorId" INTEGER NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT true,
+    "imageUrl" TEXT NOT NULL,
+    "authorId" INTEGER,
     "boardId" INTEGER,
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
@@ -19,8 +20,9 @@ CREATE TABLE "Board" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "description" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-    "authorId" INTEGER NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT true,
+    "authorId" INTEGER,
 
     CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
 );
@@ -50,13 +52,13 @@ CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
-ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Board" ADD CONSTRAINT "Board_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Board" ADD CONSTRAINT "Board_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
