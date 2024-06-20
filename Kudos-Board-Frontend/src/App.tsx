01bedd80c249964df00e81c6a5dc1a4addd3b30e
Theodore Halpern <theohal@meta.com>
2024-06-19 17:39:33 -0700

    lib: Added the mantine ui libary

--- Kudos-Board-Frontend/src/App.tsx
+++ Kudos-Board-Frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import '@mantine/core/styles.css';
 import './App.css'
+import { MantineProvider } from '@mantine/core';
 import Header from './components/pageSections/Header'
 import Footer from './components/pageSections/Footer'
 import { Route, Routes } from 'react-router-dom'
@@ -9,14 +11,14 @@ import Posts from './components/pages/Posts'
 const App = () => {
 
   return (
-    <>
+    <MantineProvider>
       <Header />
       <Routes>
         <Route index element={<Boards />} />
         <Route path="Kudos-Board/:boardId/posts" element={<Posts />} />
       </Routes>
       <Footer />
-    </>
+    </MantineProvider>
   )
 }
 
